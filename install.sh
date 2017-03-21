737eb4dc66aece4a54e2f1a4a165d33a77e6b26b
Sergey Olikh <ttauruss@gmail.com>
2017-03-21 19:39:47 +0800

    tmux.conf

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/config/vimrc ~/.vimrc
 cp ~/config/vimrc_vundle ~/.vimrc_vundle
 
+cp ~/config/tmux.conf ~/.tmux.conf
+
